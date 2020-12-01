03e489417d7147b104b0c01573e194c79eaa8f6c
Willian Alves <wba.alves@gmail.com>
2020-11-30 21:29:17 -0300

    remove import

--- src/test/java/com/group/ServicoDeVendasTest.java
+++ src/test/java/com/group/ServicoDeVendasTest.java
@@ -2,7 +2,6 @@ package com.group;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
